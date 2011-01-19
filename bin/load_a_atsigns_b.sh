48b56621e76f71b93d3e281d11d62165d257fe7f
Chris Howe <howech@ip-10-98-71-63.ec2.internal>
2011-01-19 07:24:02 +0000

    Better AAtsignsB loader

--- bin/load_a_atsigns_b.sh
+++ bin/load_a_atsigns_b.sh
@@ -1,2 +1,2 @@
-hadoop jar ../build/hbase_bulkloader.jar HbaseGraphTableBulkLoader -Dhbase.table.name=AAtsignsB -Dhbase.column.names=-,-,-,-,tweet_id,created_at,user_b_sn,rel_tw_id -Dhbase.key.indexes=1,3,2 -Dhbase.family.name=base s3://s3hdfs.infinitemonkeys.info/data/sn/tw/fixd/current/a_atsigns_b/part-00000
+hadoop jar ../build/hbase_bulkloader.jar HbaseAAtsignsBLoader -Dhbase.table.name=a_rel_b s3://s3hdfs.infinitemonkeys.info/data/sn/tw/fixd/current/a_atsigns_b/part-00000
 
