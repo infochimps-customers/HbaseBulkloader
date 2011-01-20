e56a68f716ef009e61c8e3c1e50e4ee080487548
Philip (flip) Kromer <flip@infochimps.org>
2011-01-20 00:18:30 -0600

    benford and son hashmap generator puts out actual benford distribution

--- src/benford_hashmap_generator.rb
+++ src/benford_hashmap_generator.rb
@@ -6,6 +6,34 @@ require 'configliere'; Settings.use :commandline
 #
 #   http://en.wikipedia.org/wiki/Benford's_law
 #
+# The idea is to pull off the first few characters (the "prefix") and map it to
+# a more-or-less fairly distributed
+#
+# Prefixes are strings of length prefix_chars that contain only ascii numbers.
+#
+# The typical use case for this partitioner is a keyspace with millions or more
+# possible values. For concreteness, let's say you generate the hashmap with
+# the default, prefix_chars=4.
+#
+# We have to deal with the following cases:
+#
+# * Short Portion: the key is a number less than 1000 (so, a string of fewer
+#   than 4 characters). We know what the distribution of these is: one in 1000
+#   within the short portion (1001 if you include the empty string "").
+#
+# * Main Portion: keys from 1000 to 9999. Prefixes are distributed within the
+#   main portion according to Benford's law:
+#
+#     P(n) = log10( 1 + 1.0 / n )
+#
+# In the case where there are 10,000 keys, the short portion is 10% of the main
+# portion. For 100,000 keys, it is 1%; for 100k it's 0.1%, and so forth.
+#
+# My point is, we're only going to calculate the distribution of the main
+# portion. In the actual partitioner, we're just going to lump strings from the
+# short portion into segment zero. This mildly skews the distribution if your
+# keyspace is small, but if your keyspace is small why the fuck are you using
+# this partitioner?
 #
 
 Settings.define :prefix_chars, :default =>   4, :type => Integer, :description => "How many characters you'll snip from each string. The hashmap will be about 10**prefix_chars in size (So with prefix_chars=4, have about 10,000 rows)"
@@ -16,19 +44,39 @@ GENERATED_JAVA_FILE = File.join(THIS_DIR, "BenfordAndSon.java")
 
 # ===========================================================================
 #
-# Assemble all the prefixes:
-# Strings of length 0 to prefix_chars that contain only ascii numbers.
+# Assemble a sorted list of prefixes
 #
 
 PREFIX_CHARS = ["", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def expand_prefixes prefixes
-  PREFIX_CHARS.map{|char| prefixes.map{|prefix| char + prefix }}.flatten.uniq
+  PREFIX_CHARS.map{|char| prefixes.map{|prefix| prefix + char }}.flatten.uniq
 end
 
 prefixes = [""]
 Settings.prefix_chars.times do
   prefixes = expand_prefixes(prefixes)
+end
+prefixes.sort!
+
+prefixes = (10**(Settings.prefix_chars-1) .. (10**Settings.prefix_chars - 1))
+
+# ===========================================================================
+#
+# Calculate the distribution of prefixes to segments
+#
+
+def prefixes_to_benfords_law_segments prefixes
+  benford_map = {}
+  tot_prob = 0
+  prefixes.each do |prefix|
+    prob = Math.log10( 1 + (1.0 / prefix.to_f) )
+    tot_prob += prob
+    segment = (Settings.segments * tot_prob).to_i
+    # p [prefix, prob, tot_prob, segment]
+    benford_map[prefix] = segment
+  end
+  benford_map
 end
 
 # ===========================================================================
@@ -46,7 +94,7 @@ HASHMAP_TEMPLATE = %Q{
  */
 
 class BenfordAndSon {
-  public final static Map segments = new HashMap<ImmutableBytesWriteable, Integer>() {
+  public final static Map segments = new HashMap<ImmutableBytesWritable, Integer>() {
     {
 %s
     }
@@ -57,11 +105,14 @@ class BenfordAndSon {
 # ---------------------------------------------------------------------------
 
 def java_hashmap_entry key, val
-  %Q{      put(new ImmutableBytesWriteable(Bytes.toBytes("#{key}")), #{val.to_i});}
+  %Q{      put(new ImmutableBytesWritable(Bytes.toBytes("#{key}")), #{val.to_i});}
 end
 
-$stderr.puts "Created hash map from #{prefixes.length} prefixes (all numeric strings of length #{Settings.prefix_chars} or less) to #{Settings.segments} segments; storing in #{GENERATED_JAVA_FILE}"
+$stderr.puts "Created hash map from #{prefixes.to_a.length} prefixes (all numeric strings of length #{Settings.prefix_chars} or less) to #{Settings.segments} segments; storing in #{GENERATED_JAVA_FILE}"
+prefix_to_segment = prefixes_to_benfords_law_segments(prefixes)
 File.open(GENERATED_JAVA_FILE, "w") do |generated_java_file|
-  generated_java_file.puts( HASHMAP_TEMPLATE % prefixes.map{|prefix| java_hashmap_entry(prefix, prefix) }.join("\n") )
+  hashmap_entries = prefix_to_segment.map{|prefix, segment| java_hashmap_entry(prefix, segment) }.join("\n")
+  generated_java_file.puts( HASHMAP_TEMPLATE % hashmap_entries )
 end
+
 
