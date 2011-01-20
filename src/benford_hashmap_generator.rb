575845546258186a67fb8529cee8bdbc452d3b2c
Philip (flip) Kromer <flip@infochimps.org>
2011-01-20 00:49:57 -0600

    BenfordAndSon: makes more sense to output the float distribution and do the segmenting on java side

--- src/benford_hashmap_generator.rb
+++ src/benford_hashmap_generator.rb
@@ -7,9 +7,9 @@ require 'configliere'; Settings.use :commandline
 #   http://en.wikipedia.org/wiki/Benford's_law
 #
 # The idea is to pull off the first few characters (the "prefix") and map it to
-# a more-or-less fairly distributed
-#
-# Prefixes are strings of length prefix_chars that contain only ascii numbers.
+# more-or-less fairly distributed bis.  An evenly distributed set of numbers
+# will have a Benford-distributed set of numeric strings, and will fairly evenly
+# populate each partition.
 #
 # The typical use case for this partitioner is a keyspace with millions or more
 # possible values. For concreteness, let's say you generate the hashmap with
@@ -37,7 +37,6 @@ require 'configliere'; Settings.use :commandline
 #
 
 Settings.define :prefix_chars, :default =>   4, :type => Integer, :description => "How many characters you'll snip from each string. The hashmap will be about 10**prefix_chars in size (So with prefix_chars=4, have about 10,000 rows)"
-Settings.define :segments,     :default => 144, :type => Integer, :description => "How many output segments to use; make this be well less than 10**prefix_chars."
 Settings.resolve!
 THIS_DIR = File.dirname(__FILE__)
 GENERATED_JAVA_FILE = File.join(THIS_DIR, "BenfordAndSon.java")
@@ -52,26 +51,25 @@ prefixes = (10**(Settings.prefix_chars-1) .. (10**Settings.prefix_chars - 1))
 
 # ===========================================================================
 #
-# Calculate the distribution of prefixes to segments
+# Calculate the distribution of prefixes
 #
 
-def prefixes_to_benfords_law_segments prefixes
+def prefixes_to_benfords_law_distribution prefixes
   benford_map = {}
   tot_prob = 0
   prefixes.each do |prefix|
     next if prefix == 0
     prob = Math.log10( 1 + (1.0 / prefix.to_f) )
     tot_prob += prob
-    segment = (Settings.segments * tot_prob).to_i
-    # p [prefix, prob, tot_prob, segment]
-    benford_map[prefix] = segment
+    # p [prefix, prob, tot_prob]
+    benford_map[prefix] = tot_prob
   end
   benford_map
 end
 
 # ===========================================================================
 #
-# Stuff the prefix => segment map into a java hashmap
+# Stuff the prefix => distribution map into a java hashmap
 #
 
 # ---------------------------------------------------------------------------
@@ -92,7 +90,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 
 class BenfordAndSon {
-  public final static Map segments = new HashMap<byte[], Integer>() {
+  public final static Map distribution = new HashMap<byte[], float>() {
     {
 %s
     }
@@ -103,13 +101,13 @@ class BenfordAndSon {
 # ---------------------------------------------------------------------------
 
 def java_hashmap_entry key, val
-  %Q{      put(new Bytes.toBytes("#{key}"), #{val.to_i});}
+  %Q{      put(new Bytes.toBytes("#{key}"), #{val});}
 end
 
-$stderr.puts "Created hash map from #{prefixes.to_a.length} prefixes (all numeric strings of length #{Settings.prefix_chars} or less) to #{Settings.segments} segments; storing in #{GENERATED_JAVA_FILE}"
-prefix_to_segment = prefixes_to_benfords_law_segments(prefixes)
+$stderr.puts "Created hash map from #{prefixes.to_a.length} prefixes (all numeric strings of length #{Settings.prefix_chars}) and storing it in #{GENERATED_JAVA_FILE}"
 File.open(GENERATED_JAVA_FILE, "w") do |generated_java_file|
-  hashmap_entries = prefix_to_segment.map{|prefix, segment| java_hashmap_entry(prefix, segment) }.join("\n")
+  prefix_distribution = prefixes_to_benfords_law_distribution(prefixes)
+  hashmap_entries = prefix_distribution.map{|prefix, bin| java_hashmap_entry(prefix, bin) }.join("\n")
   generated_java_file.puts( HASHMAP_TEMPLATE % hashmap_entries )
 end
 
