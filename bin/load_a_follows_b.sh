f04d18a26768704f6d7ffa837b75263eaa36b5ab
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-01-27 19:33:16 +0000

    set reduce tasks

--- bin/load_a_follows_b.sh
+++ bin/load_a_follows_b.sh
@@ -1,1 +1,1 @@
-hadoop jar build/hbase_bulkloader.jar com.infochimps.hbase.HbaseAFollowsBLoader -libjars /usr/lib/hbase/lib/guava-r05.jar /tmp/streamed/a_follows_b /tmp/hfiles/a_follows_b
+hadoop jar build/hbase_bulkloader.jar com.infochimps.hbase.HbaseAFollowsBLoader -Dmapred.reduce.tasks=360 -libjars /usr/lib/hbase/lib/guava-r05.jar /tmp/streamed/a_follows_b /tmp/hfiles/a_follows_b
